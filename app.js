1f71d04241964cbbc560493b6deb4dbe5890556d
epGameDev <46700314+epGameDev@users.noreply.github.com>
2020-03-12 20:38:58 -0500

    Added buffer and recieved messages through stream

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const http = require("http");
+const fs = require("fs");
 
 function requestListener(request, response) {
-    console.log("URL:", request.url, "Method: ",request.method, "Headers: ", request.headers);
+    console.log("URL:", request.url, "Method: ", request.method, "Headers: ", request.headers);
+
+    const url = request.url;
+    const method = request.method;
+
+    if (url === "/") {
+        response.write("<html>");
+        response.write("<head><title>Enter Message</title></head>");
+        response.write("<body>");
+        response.write(`<form action="/message" method="POST"><input type="text" name="message"></input> <button type="submit">SUBMIT</button> </form>`);
+        response.write("</body>");
+        response.write("</html>");
+        return response.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+        const reqBody = [];
+
+        request.on('data', chunk => {
+            console.log(chunk);
+            reqBody.push(chunk);
+        });
+
+        request.on("end", () => {
+            // grabs packets and concats them to text.
+            const parsedBody = Buffer.concat(reqBody).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync("message.txt", message);
+            response.statusCode = 302;
+            response.setHeader('Location', '/');
+            return response.end();
+        })
+
+    }
 
     // Sending a responce.
     response.setHeader("Content-Type", "text.html");
@@ -11,7 +45,7 @@ function requestListener(request, response) {
     response.write("<h1>Welcome To The Page!!</h1>");
     response.write("</body>");
     response.write("</html>");
-    
+
     //Finishes response and any new write method after will result in a error.
     response.end();
 
